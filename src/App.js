156602c6063b428a073a46f5a62e193cc9b6c7db
avikodre03 <akodre111@gmail.com>
2023-04-29 13:15:27 +0530

    done 06

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Comp1 from './Components/Comp1';
+import Comp2 from './Components/Comp2';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Comp1/>
+      <Comp2/>
     </div>
   );
 }
